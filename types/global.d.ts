2686d1675bacb1cabdc46cd967011e60e670fc88
Jay <jay.dbrandmaker@gmail.com>
2025-05-22 09:32:59 +0800

    fix: resolve TypeScript and build configuration issues

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,9 +1,24 @@
 import * as React from 'react';
 
 declare global {
+  namespace React {
+    interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+      // Add any custom HTML attributes here if needed
+    }
+  }
+
   namespace JSX {
     interface IntrinsicElements {
       [elemName: string]: any;
     }
+    
+    interface Element extends React.ReactElement<any, any> {}
   }
+}
+
+// Add type declarations for any missing modules
+declare module 'lodash' {
+  const _: any;
+  export = _;
+  export default _;
 }
